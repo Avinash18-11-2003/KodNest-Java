61d16944f75ff9e2a0806f5ec34beae9ab971ba2
Avinash18-11-2003 <avinashmalagi12@gmail.com>
2026-08-11 11:27:52 +0530

    Add Employee and EmployeeApplication classes

---
+++ KodNest-Java/track/m02/object oreinted/Employee.Java
@@ -0,0 +1,13 @@
+class Employee {
+    int id;
+    String name;
+    double salary;
+
+    void work() {
+        System.out.println("working");
+    }
+
+    void eat() {
+        System.out.println("eating");
+    }
+}
