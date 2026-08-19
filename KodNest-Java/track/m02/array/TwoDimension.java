b18002499de31ec2f1fe2419fe0ae612782d52f7
Avinash18-11-2003 <avinashmalagi12@gmail.com>
2026-08-19 12:02:19 +0530

    Use dynamic array lengths (ar.length and ar[i].length) for 2D array iteration and close scanner

--- KodNest-Java/track/m02/array/TwoDimension.java
+++ KodNest-Java/track/m02/array/TwoDimension.java
@@ -4,19 +4,21 @@ public class TwoDimension {
     public static void main(String[] args) {
         int [][] ar = new int[3][5];
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter the Array elements");
-        for(int i = 0; i<2; i++){
-            for(int j = 0; j<4; j++)
+        System.out.println("Enter the Array elements:");
+        for(int i = 0; i < ar.length; i++){
+            for(int j = 0; j < ar[i].length; j++)
             {
                 ar[i][j] = scan.nextInt();    
             }
         }
         System.out.println("Array Elements are:");
-        for(int i = 0; i<2; i++){
-            for(int j = 0; j<4; j++){
+        for(int i = 0; i < ar.length; i++){
+            for(int j = 0; j < ar[i].length; j++){
                 System.out.print(ar[i][j]+" ");
             }
             System.out.println();
         }
+        scan.close();
     }
 }
+
